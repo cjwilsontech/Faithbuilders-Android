85731d7d262e5442e2e78ef1e9989863ef86e202
Curtis Wilson <cj@wilsontech.com>
2019-07-09 00:19:33 -0700

    Remove map from class detail view.

--- app/src/main/java/com/eusebeia/faithbuilders/ScheduleDetailFragment.java
+++ app/src/main/java/com/eusebeia/faithbuilders/ScheduleDetailFragment.java
@@ -1,18 +1,7 @@
 package com.eusebeia.faithbuilders;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.InflateException;
 import android.view.LayoutInflater;
@@ -21,32 +10,14 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 /**
  * Created by CJ on 7/6/2016.
  */
-public class ScheduleDetailFragment extends DialogFragment implements OnMapReadyCallback, GoogleMap.OnMapLoadedCallback, LocationListener {
+public class ScheduleDetailFragment extends DialogFragment {
     private ScheduleItem scheduleItem;
-    private SupportMapFragment mapFragment;
-    private GoogleMap map;
-    private LocationManager locationManager;
-    private LatLng destinationLatLng;
-    private LatLng currentLatLng;
-    private boolean mapIsLoaded;
-    private boolean locationRequested;
     private static View view;
 
-    public ScheduleDetailFragment() {
-        mapIsLoaded = false;
-        locationRequested = false;
-    }
+    public ScheduleDetailFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -130,183 +101,4 @@ public class ScheduleDetailFragment extends DialogFragment implements OnMapReady
 
         return view;
     }
-
-    public void onDestroyView() {
-        super.onDestroyView();
-        if (mapFragment != null && mapFragment.isResumed()) {
-            getActivity().getSupportFragmentManager().beginTransaction().remove(mapFragment).commit();
-        }
-        if (locationManager != null && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.removeUpdates(this);
-        }
-    }
-
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        FragmentManager fm = getChildFragmentManager();
-        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentByTag("mapFragment");
-        if (mapFragment == null) {
-            mapFragment = new SupportMapFragment();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.add(R.id.schedule_detail_map, mapFragment, "mapFragment");
-            ft.commit();
-            fm.executePendingTransactions();
-        }
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-        // Find which building we need to show coordinates for.
-        switch (scheduleItem.getLocation()) {
-            case "Auditorium":
-                destinationLatLng = new LatLng(47.14609, -122.441857);
-                break;
-            case "Xavier":
-                destinationLatLng = new LatLng(47.146528, -122.441342);
-                break;
-            case "Admin 101":
-                destinationLatLng = new LatLng(47.147106, -122.441691);
-                break;
-            case "Music Center":
-            case "Music 306A":
-                destinationLatLng = new LatLng(47.146566, -122.445116);
-                break;
-            case "Ingram 100":
-                destinationLatLng = new LatLng(47.147284, -122.444360);
-                break;
-            case "University Center":
-                destinationLatLng = new LatLng(47.145293, -122.440437);
-                break;
-        }
-
-        // Configure the map.
-        map.addMarker(new MarkerOptions().position(destinationLatLng).title(scheduleItem.getLocation()));
-        map.moveCamera(CameraUpdateFactory.newLatLng(destinationLatLng));
-        map.setBuildingsEnabled(true);
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-        map.setOnMapLoadedCallback(this);
-
-        // Check for location permission before attempting to access it.
-        onLocationGranted();
-
-        // Show the campus view.
-        map.moveCamera(CameraUpdateFactory.newLatLng(destinationLatLng));
-        map.animateCamera(CameraUpdateFactory.zoomTo(16));
-    }
-
-    private boolean requestLocation() {
-        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            onLocationGranted();
-            return true;
-        } else if (!locationRequested) {
-            // Request location, but only do so once per screen.
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            locationRequested = true;
-        }
-        return false;
-    }
-
-    private void onLocationGranted() {
-        if (map != null) {
-            try {
-                map.setMyLocationEnabled(true);
-
-                // Configure location.
-                if (locationManager == null) {
-                    locationManager = (LocationManager) getActivity().getSystemService(getActivity().LOCATION_SERVICE);
-                }
-
-                Criteria criteria = new Criteria();
-                String bestProvider = locationManager.getBestProvider(criteria, true);
-                Location location = locationManager.getLastKnownLocation(bestProvider);
-                if (location != null) {
-                    onLocationChanged(location);
-                }
-                locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
-            } catch (SecurityException e) {
-
-            }
-        }
-    }
-
-    private void setMapViewToBounds(LatLng first, LatLng second) {
-        // Move the camera to fit both the destination and the current location.
-        LatLngBounds bounds = new LatLngBounds.Builder().include(first).include(second).build();
-        map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
-
-        if (map.getCameraPosition().zoom > 17) {
-            map.animateCamera(CameraUpdateFactory.zoomTo(17));
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        boolean isNull = currentLatLng == null;
-        currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-        // If we just got our first location and the map is ready, set the bounded view.
-        if (mapIsLoaded) {
-            setMapViewToBounds(currentLatLng, destinationLatLng);
-        }
-    }
-
-    @Override
-    public void onMapLoaded() {
-        mapIsLoaded = true;
-
-        if (currentLatLng != null) {
-            // Show the campus view relative to the current position.
-            setMapViewToBounds(currentLatLng, destinationLatLng);
-        } else {
-            // Show the campus view.
-            map.moveCamera(CameraUpdateFactory.newLatLng(destinationLatLng));
-            map.animateCamera(CameraUpdateFactory.zoomTo(16));
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission was granted.
-                    onLocationGranted();
-                }
-                return;
-            }
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (locationManager != null && ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.removeUpdates(this);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        requestLocation();
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-    }
-
 }
